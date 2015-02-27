2c8e8cadd50c081707f8317f083bfcc42bd53282
Tony Spiro <tspiro@tonyspiro.com>
2015-02-27 09:43:49 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var exec = require('child_process').exec;
 module.exports = function (options, task, cb) {
 
 	if(task == 'config'){
-		var command = "sed -e 's%$path%" + options.site_path + "%g' -e 's%$port%" + options.port + "%g' node_modules/gulp-mamp/httpd.conf-template > /Applications/MAMP/conf/apache/httpd.conf";
+		var command = "sed -e 's%user%" + options.user + "%g' -e 's%$path%" + options.site_path + "%g' -e 's%$port%" + options.port + "%g' node_modules/gulp-mamp/httpd.conf-template > /Applications/MAMP/conf/apache/httpd.conf";
 		exec(command, function(err) {
 			if (err) return cb(err); // return error
 			console.log('Configuring mamp server to point to ' + options.site_path + ' at http://localhost:'+ options.port);
